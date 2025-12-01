77e1789020df007298d6c5bc726c300339b6079a
TG <thomas.gutknecht01@gmail.com>
2025-12-01 17:31:40 +0100

    Initialized repo and finished day 1

---
+++ main.js
@@ -0,0 +1,39 @@
+//Import Modules
+const cp = require('child_process');
+const prompt = require('prompt');
+
+//Define Prompt Scheme
+const schema = {
+    properties: {
+        day: {
+            pattern: /\b(0?[1-9]|[12][0-9]|3[01])\b/g,
+            message: 'Please enter the Number of the puzzle for which you want to run the script for.',
+            required: true
+        },
+        challenge: {
+            pattern: /\b[1-2]/g,
+            message: 'There are two challenges per day, pick either 1 or 2.',
+            required: true
+        }
+    }
+};
+
+//Open Prompt
+prompt.start();
+prompt.get(schema, function (err, input) {
+
+    //Error event for prompt
+    if (err) {
+        console.log(`Error found in prompt result.\n${err}`);
+        return;
+    }
+
+    //Create path from user input
+    var path = `./puzzle${input.day}/chall${input.challenge}.js`;
+
+    //Execute script as child process
+    const result = cp.fork(path);
+
+    //Do not print the result variable as it only contains debugging data
+    //console.log(result);
+});
