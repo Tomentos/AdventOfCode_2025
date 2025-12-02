25db5ee58b73d96a55a849bcb79bb63dba400e72
TG <thomas.gutknecht01@gmail.com>
2025-12-02 17:16:21 +0100

    Added comments

--- day2/chall1.js
+++ day2/chall1.js
@@ -16,20 +16,29 @@ fs.readFile('./day2/input.txt', 'utf8', (err, data) => {
     // Define global variables
     var result = 0
 
+    //Start a counting for loop for the ranges
     for (let i = 0; i < data.length; i++) {
+
+        //Extract relevant information
         var range = data[i].split('-');
         var start = parseInt(range[0]);
         var end = parseInt(range[1]);
 
+        //Loop through every number of the range
         for (let j = start; j <= end; j++) {
+
+            //Split the value into 2 halves
             let value = String(j);
             var first = value.substring(0, value.length / 2);
             var second = value.substring(value.length / 2);
 
+            //If the halves match, add the value to the result
             if (first == second) {
                 result += parseInt(j);
             }
         }
     }
-    console.log("\n"+result);
+
+    //Print result
+    console.log(result);
 });
